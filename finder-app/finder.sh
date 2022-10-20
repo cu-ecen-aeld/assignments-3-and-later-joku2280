99feb70eb0f56aaf1d9e1becd37c15e67f83ee5c
joku2280 <joku2280@colorado.edu>
2022-10-19 22:36:36 -0400

    Comments and clean up

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 # -------------------------------------------------------
+
+
+#Check for argument count
+#exit if not correct
+[ $# -ne 2 ] && { echo "Usage: $0 filedir searchstr"; exit 1; }
+
+#Assign first argument to directory variable
 filedir="$1"
 
-searchstr="$2"
- 
-[ $# -ne 2 ] && { echo "Usage: $0 filedir searchstr"; exit 1; }
- 
+#Check if directory exsists
+#exit if not
 if [ ! -d "$filedir" ]
 then
    echo "Error: Directory $filedir does NOT exsist"
    exit 1
 fi
 
+#Assign second argument to search string variable
+searchstr="$2"
+
+#get file count
 filescount="$(grep -rl "$filedir" -e "$searchstr" | wc -l)" #xargs -0)" 
 
+#get line count
 linecount="$(grep -r "$searchstr" "$filedir" | wc -l)"
 
 echo "The number of files are $filescount and the number of matching lines are $linecount"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # -------------------------------------------------------
 
- 
+#Check for argument count
+#exit if not correct
 [ $# -ne 2 ] && { echo "Usage: $0 file writestr"; exit 1; }
 
+#Assign first argument to file variable
 file="$1"
 
+#Assign second argument to write string variable
 writestr="$2"
  
-#if [ ! -d "$file" ]
-#then
-#   echo "Error: Directory $file does NOT exsist"
-#   exit 1
-#fi
-
+#Make file/directory and write to file
 mkdir -p "$(dirname "$file")" && touch "$file"
 
-
 echo "$writestr" > "$file"
+
+#if unable to write exit with 1
+if [ $? -ne 0 ]
+then
+    echo "Unable to write to file "$file""
+    exit 1
+fi
