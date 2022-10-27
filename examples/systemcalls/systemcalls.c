01af88706a8aef99688da14ea70208fd8939d848
joku2280 <joku2280@colorado.edu>
2022-10-26 20:53:47 -0400

    Part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,11 @@
 #include "systemcalls.h"
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +23,13 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int status = system(cmd);
+
+    if(status == -1)
+    {
+        perror("Error Executing Command");
+        return false;
+    }
 
     return true;
 }
@@ -58,10 +72,24 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
+    pid_t pid;
+    pid = fork ();
+    if (pid == -1)
+        return false;
+    else if (pid == 0) {
+        execv (command[0], &command[0]);
+        exit (-1);
+    }
+
 
     va_end(args);
 
-    return true;
+    if (waitpid (pid, &status, 0) == -1)
+        return false;
+    else if (WIFEXITED (status) && (WEXITSTATUS(status) == EXIT_SUCCESS))
+        return true;    
+    return false;
 }
 
 /**
@@ -92,8 +120,34 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+	int fd = creat(outputfile,0644);
+	if(fd<0)
+	{
+		return false;
+	}
+
+    int status;
+    pid_t pid;
+    pid = fork ();
+    if (pid == -1)
+        return false;
+    else if (pid == 0) {
+        if (dup2(fd, 1) < 0) 
+        { 
+            perror("dup2"); 
+            return false; 
+        }
+        close(fd);
+        execv(command[0], &command[0]);
+        exit (-1);
+    }
+
 
     va_end(args);
 
-    return true;
+    if (waitpid (pid, &status, 0) == -1)
+        return false;
+    else if (WIFEXITED (status) && (WEXITSTATUS(status) == EXIT_SUCCESS))
+        return true;    
+    return false;
 }
